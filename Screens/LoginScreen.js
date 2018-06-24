77c742a8c01651146def969693af32f8a4602324
unknown <540376482yzb@gmail.com>
2018-06-24 00:53:27 -0700

    add fetch feature uer icons

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,54 +1,99 @@
-import React from 'react'
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
-import { View } from 'react-native'
-import { Font, AppLoading } from "expo";
+import React from 'react';
+import { Container, Header, Content, Form, Item, Input, Label, Text } from 'native-base';
+import { Button, TextInput } from 'react-native';
+import { View } from 'react-native';
+import { Font, AppLoading } from 'expo';
+import { login } from '../store/actions/authendication';
+import { connect } from 'react-redux';
 
+export class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { loading: true };
+		this.username = null;
+		this.password = null;
+	}
 
+	async componentDidMount() {
+		await Font.loadAsync({
+			Roboto: require('native-base/Fonts/Roboto.ttf'),
+			Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+		});
+		this.setState({ loading: false });
+	}
+	componentDidUpdate({ isLogin: prevIsLogin }) {
+		const { isLogin, navigation } = this.props;
+		if (!prevIsLogin && isLogin) {
+			navigation.navigate('MainTabNavigator');
+		}
+	}
+	handleLogin = () => {
+		const { dispatch, navigation } = this.props;
 
-export default class Login extends React.Component {
+		dispatch(login({ username: this.username, password: this.password }));
+	};
 
-    constructor(props) {
-        super(props);
-        this.state = { loading: true };
-    }
+	render() {
+		if (this.state.loading) return <View />;
+		return (
+			<Container style={styles.container}>
+				{this.props.errorMessage && (
+					<View style={styles.error}>
+						<Text style={styles.errorText}>{this.props.errorMessage.message}</Text>
+					</View>
+				)}
 
-    async componentWillMount() {
-        await Font.loadAsync({
-            Roboto: require("native-base/Fonts/Roboto.ttf"),
-            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-        });
-        this.setState({ loading: false });
-    }
-
-    handleLogin = (e) => {
-        e.preventDefault()
-        this.props.navigation.navigate('MainTabNavigator')
-    }
-
-    render() {
-        if (this.state.loading) return <View></View>
-        return (
-            <Container style={styles.container}>
-                <Form>
-                    <Item stackLabel>
-                        <Label>Username</Label>
-                        <Input />
-                    </Item>
-                    <Item stackLabel>
-                        <Label>Password</Label>
-                        <Input />
-                    </Item>
-                    <Button full success onPress={this.handleLogin}>
-                        <Text>Sign in</Text>
-                    </Button>
-                </Form>
-            </Container>)
-
-    }
+				<Form>
+					<Item stackLabel>
+						<Label>Username</Label>
+						<TextInput
+							onChangeText={value => (this.username = value)}
+							underlineColorAndroid="transparent"
+							style={styles.input}
+						/>
+					</Item>
+					<Item stackLabel>
+						<Label>Password</Label>
+						<TextInput
+							onChangeText={value => (this.password = value)}
+							underlineColorAndroid="transparent"
+							secureTextEntry={true}
+							style={styles.input}
+						/>
+					</Item>
+					<Button
+						title="SIGN IN"
+						color="#67BEAC"
+						accessibilityLabel="SIGN IN"
+						onPress={() => this.handleLogin()}
+					/>
+				</Form>
+			</Container>
+		);
+	}
 }
+const mapStateToProps = state => {
+	return {
+		isLogin: state.userReducer.isLogin,
+		errorMessage: state.userReducer.errorMessage,
+	};
+};
+
+export default connect(mapStateToProps)(Login);
 
 const styles = {
-    container: {
-        marginTop: 50
-    }
-}
+	container: {
+		marginTop: 50,
+	},
+	error: {
+		minHeight: 20,
+	},
+	errorText: {
+		color: 'red',
+		padding: 10,
+	},
+	input: {
+		minWidth: 200,
+		padding: 10,
+	},
+};
--- store/reducers/userReducer.js
+++ store/reducers/userReducer.js
@@ -1,12 +1,23 @@
+import { LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/authendication';
+import { FETCH_FEATURE_STARS_ICON_SUCCESS } from '../actions/users';
+
 const initialState = {
+  isLogin: false,
+  errorMessage: null,
+  featureStarsIcons: [],
+};
 
-}
 const userReducer = (state = initialState, action) => {
-    switch (action.type) {
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      return { ...state, isLogin: true };
+    case LOGIN_FAIL:
+      return { ...state, isLogin: false, errorMessage: action.error };
+    case FETCH_FEATURE_STARS_ICON_SUCCESS:
+      return { ...state, featureStarsIcons: action.icons };
+    default:
+      return state;
+  }
+};
 
-
-export default userReducer
+export default userReducer;
