7e3feed619f9ae8d987702bf23ee5d05783895e4
unknown <540376482yzb@gmail.com>
2018-06-24 01:12:21 -0700

    fetch user icon success

--- Components/FeaturedUserCell.js
+++ Components/FeaturedUserCell.js
@@ -1,68 +1,82 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import {
+  StyleSheet, Text, View, Image, TouchableOpacity, ScrollView,
+} from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation';
-import { Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon, Container, Content } from 'native-base'
-import PartnerListScreen from '../Screens/PartnerListScreen'
+import {
+  Card,
+  CardItem,
+  Thumbnail,
+  Body,
+  Left,
+  Right,
+  Button,
+  Icon,
+  Container,
+  Content,
+} from 'native-base';
+import PartnerListScreen from '../Screens/PartnerListScreen';
 
-
-
-
-class FeaturedUserCell extends Component {
-
-    render() {
-        return (
-            <Card>
-                <View>
-                    <View style={{ paddingTop: 5, paddingBottom: 5, justifyContent: 'space-between', flex: 1, flexDirection: 'row', alignItems: 'center', marginHorizontal: 12 }}>
-                        <Text>Featured Stars</Text>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('PartnerListScreen')}>
-                            <Text>View All</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ flex: 3, flexDirection: 'row', paddingBottom: 10 }}>
-                        <ScrollView horizontal={true} contentContainerStyle={{ alignItems: 'center', marginHorizontal: 12 }}>
-                            <TouchableOpacity style={{ paddingRight: 20 }} onPress={() => this.props.navigation.navigate('UserProfileScreen')}>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity >
-                            <TouchableOpacity style={{ paddingRight: 20 }}>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{ paddingRight: 20 }}>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{ paddingRight: 20 }}>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{ paddingRight: 20 }}>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{ paddingRight: 20 }}>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{ paddingRight: 20 }}>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Image source={require('../assets/allen.png')} style={{ height: 60, width: 60 }} />
-                            </TouchableOpacity>
-                        </ScrollView>
-                    </View>
-                </View>
-            </Card>
-        );
+export function FeaturedUserCell({ navigation, icons }) {
+    
+    const renderIcons = icons => {
+        return icons.map((icon,index) => {
+            return(
+                <TouchableOpacity
+                style={{ paddingRight: 20 }}
+                onPress={() => navigation.navigate('UserProfileScreen')}
+                key={index}
+            >
+                <Image source={{uri:icon}} style={styles.avatar} />
+            </TouchableOpacity>
+            )
+        })
     }
+    return (
+    <Card>
+        <View>
+        <View style={styles.viewAll}>
+            <Text>
+            Featured Stars
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('PartnerListScreen')}>
+            <Text>
+                View All
+            </Text>
+            </TouchableOpacity>
+        </View>
+        <View style={{ flex: 3, flexDirection: 'row', paddingBottom: 10 }}>
+            <ScrollView
+            horizontal
+            contentContainerStyle={{ alignItems: 'center', marginHorizontal: 12 }}
+            >
+            {
+                icons && renderIcons(icons)
+            }
+            </ScrollView>
+        </View>
+        </View>
+    </Card>
+    );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 10,
-        borderRadius: 4,
-        height: 96,
-        alignItems: 'center',
-    },
-    CardView: {
-
-    }
-})
+  container: {
+    flex: 10,
+    borderRadius: 4,
+    height: 96,
+    alignItems: 'center',
+  },
+  viewAll: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 12,
+  },
+  avatar:{ height: 60, width: 60 , borderRadius:60}
+});
 
 export default FeaturedUserCell;
